a788defcb15f74229720690a44be469c1b83c9c3
tharakamd <tharakamd.12@cse.mrt.ac.lk>
2016-12-01 14:52:56 +0530

    add variables to pipeline

--- PipeLine.py
+++ PipeLine.py
@@ -2,5 +2,27 @@ from AbstractPipeLine import AbstractPipeLine
 
 class PipeLine(AbstractPipeLine):
 
-    def addElement(self):
-        return
+    preProcessorList = []
+    imageProcessorList = []
+    outputGeneratorList = []
+
+    def initPipeLine(self):
+        global preProcessorList
+        global imageProcessorList
+        global outputGeneratorList
+        preProcessorList = []
+        imageProcessorList = []
+        outputGeneratorList = []
+
+    def addPreProcessor(self,preprocessor):
+        global preProcessorList
+        preProcessorList.append(preprocessor)
+
+    def addImageProcessor(self,imageProcessor):
+        global imageProcessorList
+        imageProcessorList.append(imageProcessor)
+
+    def addOutputGeneratorList(self,outputGenerator):
+        global outputGeneratorList
+        outputGeneratorList.append(outputGenerator)
+
