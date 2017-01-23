da11c28e4cc24c204be3a52e7bdc35f36575e284
tharakamd <tharakamd.12@cse.mrt.ac.lk>
2017-01-23 18:15:28 +0530

    add heuristic based evaluation to the distance helper class

--- Sensors/DistanceHelper.py
+++ Sensors/DistanceHelper.py
@@ -1,8 +1,14 @@
 from Gyroscope import Gyroscope
 class DistanceHelper:
+    MAX_SPEED = 0.8
+    AVERAGE_SPEED = 0.55
 
     def __init__(self):
         self.gyro = Gyroscope()
 
     def getCurrentVelocity(self):
-        return 2
+        current_speed = self.gyro.getCurrentVelocity();
+        if(current_speed > self.MAX_SPEED): # error in speed, then give average speed
+            return self.AVERAGE_SPEED
+        return current_speed
+
